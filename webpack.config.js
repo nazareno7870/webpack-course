5d2b6f771de58442ee8cbae9e7e7cd3a867d8179
Nazareno <nazareno7870@gmail.com>
2022-05-25 10:04:48 -0300

    stylus loader config

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                test: /\.css|.styl$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
             }
         ]
     },
