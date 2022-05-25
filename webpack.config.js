8b00bd5fff3318f6363fd0589600910ff0d3e7ec
Nazareno <nazareno7870@gmail.com>
2022-05-25 10:12:58 -0300

    loader images config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             {
                 test: /\.css|.styl$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
